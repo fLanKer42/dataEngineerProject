a0b723847503e9435b2a17174322894b4a0f93f3
Dhruv Rai <58771543+fLanKer42@users.noreply.github.com>
2023-02-22 02:55:32 +0530

    changes to readXL.py

--- readXL.py
+++ readXL.py
@@ -11,26 +11,11 @@ count = 0
 sheetData = list()
 for row in range(4, dataframe1.max_row - 2):
     c = 0
-    data = dict()
+    data = list()
+    data.append("2023")
+    data.append("JAN")
     for col in dataframe1.iter_cols(2, dataframe1.max_column):
         #print(col[row].value, end=" : ")
-        if (c == 1):
-            data["BANK NAME"] = col[row].value
-        elif (c == 2):
-            data["TOTAL OUTWARD DEBITS :: NO. OF OUTWARD TRANSACTIONS"] = col[row].value
-        elif (c == 3):
-            data["TOTAL OUTWARD DEBITS :: AMOUNT (LAKHS)"] = col[row].value
-        elif (c == 4):
-            data["RECIEVED INWARD CREDITS :: NO. OF INWARD TRANSACTIONS"] = col[row].value
-        elif (c == 5):
-            data["RECIEVED INWARD CREDITS :: AMOUNT (LAKHS)"] = col[row].value
-        c+=1
+        data.append(col[row].value)
     #print(" ")
     sheetData.append(data)
-count = 0
-for row in sheetData:
-    if count > 10:
-        break
-    count+=1
-    for key in row:
-        print(key + " : " + str(row[key]))
